8dc87a29b403c0c71c1df369e1c73fa4c040acd6
Em3raldz <101809224+zypeaLLas@users.noreply.github.com>
2026-07-11 16:13:31 +0700

    Add: remove-duplicates-from-sorted-array - submission-11

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-11.py
@@ -0,0 +1,27 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        # i=0
+        # while i < len(nums)-1:
+        #     j=i+1
+        #     while j < len(nums):
+        #         if nums[i] == nums[j]:
+        #             nums.pop(j)
+        #         else: #need else, we can't just i+=1 after popping -> missing an element.
+        #             # j+=1  sorted already so if it's different -> all the next elements are different. so break.
+        #             break
+        #     i+=1
+        # return len(nums)
+        #####SORTED SOLUTION
+        # unique = sorted(set(nums))
+        # nums[:len(unique)] = unique
+        # return len(unique)
+        """Two pointers sol"""
+        n = len(nums)
+        l = r = 0
+        while r < n:
+            nums[l] = nums[r]
+            while r < n and nums[r] == nums[l]:
+                r += 1
+            l += 1
+        return l
